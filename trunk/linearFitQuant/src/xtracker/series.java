4336d71478b7370c7721087188b78b8744bcf9cf
luca.bianco78@gmail.com <luca.bianco78@gmail.com@f6bc5179-6f08-a4ae-c3c1-3074e8726f77>
2011-06-02 09:08:14 +0000

    data structures now included as library

--- trunk/linearFitQuant/src/xtracker/series.java
+++ trunk/linearFitQuant/src/xtracker/series.java
@@ -1,36 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/*  * To change this template, choose Tools | Templates  * and open the template in the editor.  */
 package xtracker;
-
 import java.util.*;
-
-/**
- *
- * @author luca
- */
+/**  *  * @author luca  */
 public class series {
-    
     series(float rtVal, float icVal){
         RT=rtVal;
         IC=icVal;
     }
-
-
-   
-
     public float getRT(){
         return RT;
     }
-
     public float getIC(){
         return IC;
     }
-
     public float RT;
-
     public float IC;
-
 }
